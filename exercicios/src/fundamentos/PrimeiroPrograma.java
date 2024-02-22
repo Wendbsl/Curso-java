d658bb93520ef2c4c8ce676f2587bdef8c3a7869
Wende <Wende@192.168.0.14>
2024-02-22 17:29:14 -0300

    adicionando comentários e JAVADOC

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,9 +1,17 @@
 package fundamentos;
 
+/**
+ * @author Wendell Boldrini
+ * @since JDK1.0
+ */
 public class PrimeiroPrograma {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// isso é um comentário
+		// Sentença de código termina com ;
 		System.out.println("Primeiro Programa Parte #01!");
 		System.out.println("Primeiro Programa Parte #02!");
 
